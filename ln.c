c32cb82b4851b7cc4b4869dddaa7829f720ecca1
Adrian Statescu <mergesortv@gmail.com>
2016-08-02 20:41:11 -0700

    version c

---
+++ ln.c
@@ -0,0 +1,50 @@
+/**
+ *         x 
+ * f(x) = e - a
+ */
+
+#include <stdio.h>
+#include <math.h>
+#define EPS 0.00001
+
+
+double DivideEtImpera(double n, double lo, double hi) {
+
+       if( fabs(lo - hi) < EPS ) {
+
+           return (lo + hi)/2.0; 
+
+       } else if( (exp(lo) - n) * ( exp( (lo + hi)/2.0 ) - n ) < 0 ) {
+
+              return DivideEtImpera(n, lo, ( (lo + hi)/2.0 ));
+
+       } else {
+
+              return DivideEtImpera(n, ( (lo + hi)/2.0 ), hi);
+       }
+}
+
+double ln(double n) {
+
+       if( n < 0 ) return;
+
+       if( n == 0) return 1; 
+
+        else 
+
+          return DivideEtImpera(n, 0, n); 
+       
+}
+
+int main() {
+
+    float n;
+
+    printf("n=");
+    scanf("%f", &n);
+
+    printf("ln(%.1f) = %.5f\n", n, ln(n));
+    printf("ln(%.1f) = %.5f", log(n));
+
+ return(0);  
+}
